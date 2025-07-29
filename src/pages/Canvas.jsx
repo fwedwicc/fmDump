b97efe130c2182214027f1b926f4167e37e6d0af
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-07-30 00:28:15 +0800

    Refactor Canvas component layout and styles; enhance button design and spacing

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -3,29 +3,37 @@ import React from 'react'
 const Canvas = () => {
   return (
     <section className='relative h-screen overflow-hidden bg-neutral-100 grid place-content-center'>
-      <div className='flex flex-col gap-y-10 bg-white w-sm rounded-4xl p-7 shadow-2xl shadow-neutral-900/20'>
+      <div className='flex flex-col gap-y-14 bg-white w-sm rounded-[38px] p-7 shadow-2xl shadow-neutral-400/60'>
         <div className='flex justify-between items-center'>
-          <div className='size-10 border rounded-lg'></div>
-          <div className='size-10 border rounded-lg'></div>
+          <div className='size-12 bg-neutral-800 rounded-xl'></div>
+          <div className='relative size-12 rounded-xl shadow-lg shadow-neutral-200/60 overflow-hidden'>
+            <span className='absolute top-0 bg-neutral-800 w-full text-[9px] py-[2.5px] text-center text-neutral-200'>JAN</span>
+          </div>
         </div>
         <div className='flex flex-col gap-y-1.5'>
-          <span className='text-base font-semibold text-neutral-700'>Apple <span className='text-xs font-thin text-neutral-400'>7 days ago</span></span>
+          <span className='text-base font-medium text-neutral-700'>Apple <span className='text-[11px] font-thin text-neutral-400'>7 days ago</span></span>
           <span className='text-xl font-semibold text-neutral-700'>Senior Design Engineer</span>
           <div className='mt-2 flex gap-2'>
-            <span className='px-2.5 py-1 text-xs bg-neutral-200/80 rounded-lg text-neutral-600 font-medium'>Full time</span>
-            <span className='px-2.5 py-1 text-xs bg-neutral-200/80 rounded-lg text-neutral-600 font-medium'>Hybrid mode</span>
+            <span className='px-2 py-1.5 text-[11px] leading-none bg-neutral-200/70 rounded-lg text-neutral-600 font-medium'>Full-time</span>
+            <span className='px-2 py-1.5 text-[11px] leading-none bg-neutral-200/70 rounded-lg text-neutral-600 font-medium'>Hybrid mode</span>
           </div>
         </div>
-        <button className='bg-neutral-800 p-[5px] rounded-3xl shadow-xl shadow-neutral-500/50'>
-          <div className='w-full p-[1px] bg-neutral-900 rounded-[20px]'>
-            <div className='relative w-full p-3 bg-neutral-900 rounded-[20px] text-lg text-neutral-300/80 text-shadow-lg shadow-black'>
-              <div className='absolute w-[87%] top-2 h-2 bg-neutral-200/80 blur-sm' />
-              <div className='absolute w-1 right-2 top-2 h-10 bg-neutral-200/30 blur-xs' />
-              <div className='absolute w-1 right-2 top-2 h-10 bg-neutral-300/30 blur-sm' />
-              Apply now
-            </div>
+        <div className='flex flex-col gap-y-7 mt-5'>
+          <div className='flex justify-between items-end pb-3.5 border-b border-neutral-200'>
+            <span className='text-xl font-medium text-neutral-700'>$480k <span className='text-xs font-thin'>/year</span></span>
+            <span className='text-sm font-medium text-neutral-700'>Bay Area, CA</span>
           </div>
-        </button>
+          <button className='bg-gradient-to-r from-neutral-900/90 to-neutral-700/90 p-[3.3px] rounded-[22px] shadow-xl shadow-neutral-500/50'>
+            <div className='w-full p-[1px] rounded-[19px] gradient-bg'>
+              <div className='relative w-full p-3 bg-gradient-to-r from-neutral-900 to-neutral-900/80 rounded-[17px] text-lg text-neutral-300/90 text-shadow-lg shadow-black'>
+                <div className='absolute w-[87%] top-2 h-2 bg-neutral-200/80 blur-sm' />
+                <div className='absolute w-1 right-2 top-2 h-10 bg-neutral-200/30 blur-xs' />
+                <div className='absolute w-1 right-2 top-2 h-10 bg-neutral-300/30 blur-sm' />
+                Apply now
+              </div>
+            </div>
+          </button>
+        </div>
       </div>
     </section>
   )
