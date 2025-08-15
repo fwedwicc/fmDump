c336d9eae860de837eb2d7e31b69f548faf9b2dc
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-08-15 19:50:20 +0800

    Implement click outside to close menu in Header component; refactor social links into a separate function for better organization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import FMLogo from '/fm-logo.png'
 import { TbBrandLinkedin, TbBrandDribbble, TbBrandBehance, TbBrandGithub, TbLink } from "react-icons/tb"
 
 const Header = () => {
+
+  const [openMenu, setOpenMenu] = useState(false)
+  const menuRef = useRef(null)
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setOpenMenu(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+
+  const Socials = () => {
+    return (
+      <>
+        <a href='https://buymeacoffee.com/fwedwicc' target='_blank' rel='noopener noreferrer' className='button-base flex-center button-primary'>Buy me a coffee</a>
+        <div className='flex gap-1'>
+          {[
+            { icon: TbBrandGithub, link: 'https://github.com/fwedwicc' },
+            { icon: TbBrandDribbble, link: 'https://dribbble.com/fwedwic' },
+            { icon: TbBrandBehance, link: 'https://www.behance.net/fwedwicc' },
+            { icon: TbBrandLinkedin, link: 'https://www.linkedin.com/in/frederick-moreno/' },
+          ].map((item, index) => (
+            <a href={item.link} target='_blank' rel='noopener noreferrer' className='hover:bg-zinc-200/30 transition-smooth p-2.5 grid place-content-center rounded-xl' key={index}>
+              <item.icon className='size-4 text-zinc-600' />
+            </a>
+          ))}
+        </div>
+      </>
+    )
+  }
+
   return (
     <header className='fixed z-50 top-0 w-full md:p-4 p-2 bg-zinc-100/80 backdrop-blur-xl'>
       <div className='flex justify-between items-start gap-6'>
@@ -21,23 +59,28 @@ const Header = () => {
           </div>
         </div>
         {/* Right Content */}
-        <button className='md:hidden flex'>
-          <TbLink className='size-6 stroke-[2px] text-neutral-800' />
-        </button>
+        <div className='relative' ref={menuRef}>
+          <AnimatePresence>
+            {openMenu && (
+              <motion.div
+                initial={{ opacity: 0, y: -8, scale: 0.95 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                exit={{ opacity: 0, y: -8, scale: 0.95 }}
+                transition={{ type: 'spring', stiffness: 270, damping: 14, bounce: 0.45 }}
+                className="absolute right-1 top-9 bg-white border border-neutral-200/50 rounded-[16px] p-1 space-y-2 shadow-xl shadow-neutral-400/10 z-50"
+              >
+                <Socials />
+              </motion.div>
+            )}
+          </AnimatePresence>
+          {/* Link Toggle Button */}
+          <button className='md:hidden flex' onClick={() => setOpenMenu(!openMenu)}>
+            <TbLink className='size-6 stroke-[2px] text-neutral-800' />
+          </button>
+        </div>
+        {/* MD to LG Screens: Social Links */}
         <div className='md:flex hidden flex-col items-end gap-3'>
-          <a href='https://buymeacoffee.com/fwedwicc' target='_blank' rel='noopener noreferrer' className='button-base button-primary'>Buy me a coffee</a>
-          <div className='flex gap-1'>
-            {[
-              { icon: TbBrandGithub, link: 'https://github.com/fwedwicc' },
-              { icon: TbBrandDribbble, link: 'https://dribbble.com/fwedwic' },
-              { icon: TbBrandBehance, link: 'https://www.behance.net/fwedwicc' },
-              { icon: TbBrandLinkedin, link: 'https://www.linkedin.com/in/frederick-moreno/' },
-            ].map((item, index) => (
-              <a href={item.link} target='_blank' rel='noopener noreferrer' className='hover:bg-zinc-200/30 transition-smooth p-2.5 grid place-content-center rounded-xl' key={index}>
-                <item.icon className='size-4 text-zinc-600' />
-              </a>
-            ))}
-          </div>
+          <Socials />
         </div>
       </div>
     </header>
