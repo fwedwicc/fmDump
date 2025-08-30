021d660614839784da497f3dc0bc1f692f7488b7
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-08-30 21:57:22 +0800

    feat: add UI004 component with flight and driving information display
    - Implemented UI004 component featuring circular progress indicators for walking, flight, and driving.
    - Added visual elements including icons, gradients, and responsive design.
    - Created index.js to export UI004 and its raw code for further use.

---
+++ src/components/dumps/004/index.js
@@ -0,0 +1,7 @@
+import UI004 from './UI004.jsx'
+import UI004Code from './UI004.jsx?raw'
+
+export {
+  UI004,
+  UI004Code
+}
--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,10 @@
 // Thumbnail Imports
-import { Thumbnail001, Thumbnail002, Thumbnail003 } from '../assets/thumbnails'
+import { Thumbnail001, Thumbnail002, Thumbnail003, Thumbnail004 } from '../assets/thumbnails'
 // Data Imports
 import { UI001, UI001Code, UI001CSS } from '../components/dumps/001'
 import { UI002, UI002Code } from '../components/dumps/002'
 import { UI003, UI003Code } from '../components/dumps/003'
+import { UI004, UI004Code } from '../components/dumps/004'
 
 export const Data = [
   {
@@ -39,5 +40,16 @@ export const Data = [
     UI: UI003,
     sourceCodeJSX: UI003Code,
     credits: 'https://ph.pinterest.com/pin/3799980930358509',
+  },
+  {
+    id: 'ui004',
+    to: '/view/ui004',
+    thumbnail: Thumbnail004,
+    title: '004',
+    label: 'Flight Booking Widget',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestiae corrupti ipsam fugit velit asperiores.',
+    UI: UI004,
+    sourceCodeJSX: UI004Code,
+    credits: 'https://dribbble.com/shots/25049627-UI-Cards-Animations-For-Booking-Platform',
   },
 ]
