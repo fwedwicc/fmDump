d82a61e9a51a6c3883cf3e4b193f6c68d439bbfb
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-08-31 13:15:34 +0800

    refactor: enhance Canvas layout and styling; add custom shadow and heading font

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -12,21 +12,32 @@ const Canvas = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.4, ease: 'easeOut' }}
-      className='relative h-screen overflow-hidden bg-neutral-100 grid place-content-center'
+      className='relative h-screen overflow-hidden bg-amber-100/40 grid place-content-center'
     >
       {/* ------------------------ PLAYGROUND ------------------------ */}
-      <div className='w-[24rem] rounded-[18px] p-[2.5px] bg-stone-800'>
-        <div className='relative h-88 rounded-t-[15.5px] overflow-hidden w-full'>
-          <img src="https://i.pinimg.com/1200x/1d/f5/35/1df5351ca6a2361c142a9e80c35ca36b.jpg" alt="" className='absolute object-cover size-full' />
-        </div>
-        <div className='space-y-12 p-4'>
-          <div>
-            <span className='text-sm text-stone-300'>Transform your digital presence</span>
-            <span className='text-sm text-stone-300'>Transform your digital presence</span>
+      <div className='relative'>
+        <div className='absolute bg-stone-800/30 h-[100%] w-[95%] mt-5 -left-5 z-10 blur-xl' />
+        <div className='absolute bg-stone-800/10 h-[105%] w-[110%] mt-12 -left-24 z-10 blur-xl' />
+        <div className='relative w-[23rem] rounded-[18px] p-[2.5px] bg-stone-900 custom-shadow z-50'>
+          <div className='relative h-[20rem] rounded-t-[15.5px] overflow-hidden w-full'>
+            <img src="https://i.pinimg.com/1200x/1d/f5/35/1df5351ca6a2361c142a9e80c35ca36b.jpg" alt="" className='absolute object-cover size-full' />
           </div>
-          <div className='flex items-end justify-between'>
-            <span className='leading-none block text-sm text-stone-300'>Transform</span>
-            <span className='leading-none block text-sm text-stone-300'>Transform</span>
+          <div className='space-y-12 p-4'>
+            <div>
+              <span className='block text-[22px] text-stone-300/90 heading-font'>Transform your digital presence.</span>
+              <span className='block text-[10px] text-stone-500'>From zero to extraordinary. Let's create your digital reality.</span>
+              <button className='mt-1.5 relative px-2.5 py-2 bg-gradient-to-b from-stone-300/10 via-stone-900 to-stone-900 border border-stone-200/20 rounded-full text-[10px] text-stone-400 leading-none'>
+                Send a message
+              </button>
+            </div>
+            <div className='flex items-end justify-between'>
+              <span className='leading-none block text-[11px] text-stone-400'>Transform</span>
+              <div className='flex items-center gap-1'>
+                <span className='leading-none block text-[11px] text-stone-400'>Transform</span>
+                <span className='leading-none block text-[11px] text-stone-400'>Transform</span>
+                <span className='leading-none block text-[11px] text-stone-400'>Transform</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
