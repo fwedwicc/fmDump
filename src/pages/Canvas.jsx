5938c6af12a93d0edc2f5e3b1f9e2a22c45d1d42
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-12-21 19:48:59 +0800

    feat: update Canvas component with new icons and improved layout structure

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { Tooltip } from '../components'
-import { TbSmartHome } from "react-icons/tb"
+import { TbSmartHome, TbReload, TbWifi } from "react-icons/tb"
 
 // Recording Device
 const Canvas = () => {
@@ -15,7 +15,7 @@ const Canvas = () => {
     >
       {/* ------------------------ PLAYGROUND ------------------------ */}
       <div className='flex w-[23rem] p-[0.2px] pb-3 rounded-[21px] border'>
-        <div className='flex flex-col justify-end gap-4 items-end size-full rounded-[20.8px] p-1.5 border'>
+        <div className='flex flex-col justify-end gap-4 items-end size-full rounded-[20.8px] p-2 bg-neutral-200/60 border'>
           {/*  */}
           <div className='relative flex flex-col items-center justify-between p-5 h-85 rounded-[16px] w-full bg-neutral-800 border'>
             <div className='flex flex-col gap-1 absolute top-1/2 -translate-y-1/2 w-[98.5%] h-[98.5%] rounded-[14px] overflow-hidden border border-neutral-600'>
@@ -24,7 +24,7 @@ const Canvas = () => {
               ))}
             </div>
             <div className='flex justify-between w-full border border-neutral-500'>
-              <div className='flex gap-7 border border-neutral-500'>
+              <div className='flex gap-7 border border-neutral-500 relative z-20'>
                 <p className='text-neutral-400 leading-none text-[10px]'>11:11 PM</p>
                 <div className='flex items-center gap-1 border border-neutral-500'>
                   <div className='size-1.5 rounded-full bg-red-400 border' />
@@ -41,23 +41,37 @@ const Canvas = () => {
               <span className='text-neutral-100 text-4xl font-medium leading-none'>00:30:31</span>
             </div>
           </div>
-          <div className='flex flex-col gap-4 p-1.5 border'>
-            {/*  */}
-            <div className='flex gap-2 border w-full'>
-              <div className='w-12 border rounded-lg'></div>
-              <div className='grid grid-cols-41 gap-x-[3px] gap-y-[2px] border'>
-                {Array.from({ length: 164 }, (_, i) => (
-                  <div key={i} className='size-1 rounded-full border' />
-                ))}
+          {/*  */}
+          <div className='flex gap-2 border w-full'>
+            <div className='w-12 border rounded-lg'></div>
+            <div className='grid grid-cols-42 gap-x-[3px] gap-y-[2px] border'>
+              {Array.from({ length: 168 }, (_, i) => (
+                <div key={i} className='size-1 rounded-full border' />
+              ))}
+            </div>
+          </div>
+          {/*  */}
+          <div className='w-full grid grid-cols-3 gap-[1.5px] p-[1.5px] rounded-2xl bg-neutral-900 overflow-hidden h-34'>
+            <div className='flex items-center justify-center bg-[#e7e7e7] border-t-[1px] border-white rounded-l-[15px] rounded-r-sm'>
+              <div className='size-13 flex items-center justify-center rounded-full border'>
+                <div className='size-4 bg-red-600/50 rounded-full border' />
               </div>
             </div>
-            {/*  */}
-            <div className='w-full grid grid-cols-3 gap-[1.5px] p-[1.5px] rounded-2xl bg-neutral-900 overflow-hidden h-28'>
-              <div className='bg-neutral-100 rounded-l-[15px] rounded-r-sm'></div>
-              <div className='bg-neutral-100 rounded-sm'></div>
-              <div className='flex flex-col gap-[1.5px]'>
-                <div className='flex-1 bg-neutral-100 rounded-sm rounded-tr-[15px]'></div>
-                <div className='flex-1 bg-neutral-100 rounded-sm rounded-br-[15px]'></div>
+            <div className='flex items-center justify-center bg-[#e7e7e7] border-t-[1px] border-white rounded-sm'>
+              <div className='size-13 flex items-center justify-center rounded-full border'>
+                <div className='size-3.5 bg-neutral-500 rounded-[2px] border' />
+              </div>
+            </div>
+            <div className='flex flex-col gap-[1.5px]'>
+              <div className='flex items-center justify-center flex-1 bg-[#e7e7e7] border-t-[1px] border-white rounded-sm rounded-tr-[15px]'>
+                <div className='size-10 flex items-center justify-center rounded-full border'>
+                  <TbWifi />
+                </div>
+              </div>
+              <div className='flex items-center justify-center flex-1 bg-[#e7e7e7] border-t-[1px] border-white rounded-sm rounded-br-[15px]'>
+                <div className='size-10 flex items-center justify-center rounded-full border'>
+                  <TbReload />
+                </div>
               </div>
             </div>
           </div>
