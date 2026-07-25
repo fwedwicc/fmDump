55bad247ad6d644c2a1529638ebd56b99e170eaf
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-07-25 19:41:32 +0800

    refactor: remove CodedexPlayground component and related ProgressBar functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Home from './Home'
 import ViewUI from './ViewUI'
 import Canvas from './Canvas'
-import CodedexPlayground from './CodedexPlayground'
 
 export {
   Home,
   ViewUI,
-  Canvas,
-  CodedexPlayground
+  Canvas
 }
