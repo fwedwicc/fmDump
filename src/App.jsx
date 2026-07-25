9b0fe7d1299f38a787aa23747821bf8020ece81f
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-07-26 01:22:49 +0800

    Added lenis hook for smooth scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home, ViewUI, Canvas } from './pages'
+import useLenis from './hooks/useLenis'
 
 const App = () => {
+  useLenis()
   return (
     <Router>
       <Routes>
