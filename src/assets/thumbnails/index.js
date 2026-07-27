17f359c251871c9bf8182a2d1b96f45653f52c5a
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-07-27 16:12:04 +0800

    Change thumbnails to webp for more optimized image in prod

--- src/assets/thumbnails/index.js
+++ src/assets/thumbnails/index.js
@@ -1,8 +1,8 @@
-import Thumbnail001 from './001-thumbnail.png'
-import Thumbnail002 from './002-thumbnail.png'
-import Thumbnail003 from './003-thumbnail.png'
-import Thumbnail004 from './004-thumbnail.png'
-import Thumbnail005 from './005-thumbnail.png'
+import Thumbnail001 from './001-thumbnail.webp'
+import Thumbnail002 from './002-thumbnail.webp'
+import Thumbnail003 from './003-thumbnail.webp'
+import Thumbnail004 from './004-thumbnail.webp'
+import Thumbnail005 from './005-thumbnail.webp'
 
 export {
   Thumbnail001,
