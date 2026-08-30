78b936bbe5292e909aa62209ed1b6d9ec0cdd208
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-08-30 17:38:47 +0800

    progress for gardening UI (3)

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -16,23 +16,38 @@ const Canvas = () => {
     >
       {/* ------------------------ PLAYGROUND ------------------------ */}
       <div className='relative grid grid-cols-2 gap-0.5 w-full min-w-md h-130 rounded-3xl overflow-hidden bg-gradient-to-b from-[#EAEBEB] to-[#878788] shadow-2xl shadow-neutral-950/60'>
-        <div className='flex justify-start size-full rounded-l-3xl py-0.75 pl-0.75 rounded-r-sm bg-neutral-600'>
+        <div className='flex justify-start size-full rounded-l-3xl py-0.5 pl-0.5 rounded-r-sm bg-neutral-600'>
           <div className='h-full bg-neutral-800 rounded-l-[22px] rounded-r-sm w-51' />
         </div>
-        <div className='flex justify-end size-full rounded-r-3xl py-0.75 pr-0.75 rounded-l-sm bg-neutral-600'>
+        <div className='flex justify-end size-full rounded-r-3xl py-0.5 pr-0.5 rounded-l-sm bg-neutral-600'>
           <div className='h-full bg-neutral-800 rounded-r-[22px] rounded-l-sm w-51' />
         </div>
         <div className='absolute w-full h-full p-1'>
-          <div className='size-full bg-neutral-800 rounded-2xl p-1.5'>
+          <div className='size-full bg-neutral-800 rounded-[24px] p-1.5'>
             <div className='flex flex-col size-full bg-neutral-950 rounded-[16px] overflow-hidden'>
-              <div className='relative w-full h-full flex-1 flex-col bg-amber-500'>
-                <img src='https://plus.unsplash.com/premium_photo-1728566648330-7ba0d675f3c6?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='sample' className='size-full object-cover absolute'></img>
-                <div className='absolute h-4 w-full bg-black'>
-
+              <div className='relative w-full h-full flex-1 flex-col'>
+                <img src='https://plus.unsplash.com/premium_photo-1728566648330-7ba0d675f3c6?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='sample' className='size-full object-cover absolute z-40'></img>
+                <div className='absolute -top-5.5 -translate-x-1/2 left-1/2  bg-neutral-950 h-17 w-[120%] z-40 blur-md' />
+                <div className='absolute -top-5.5 -translate-x-1/2 left-1/2  bg-neutral-950/90 h-30 w-[120%] z-40 blur-2xl' />
+                <div className='flex items-center justify-between absolute py-1.5 px-2.25 w-full bg-neutral-950 z-50'>
+                  <p className='text-white text-[9px] leading-none'>14:19</p>
+                  <div className='flex items-center gap-2'>
+                    <p className='text-white text-[9px] leading-none'>.</p>
+                    <p className='text-white text-[9px] leading-none'>.</p>
+                    <p className='text-white text-[9px] leading-none'>.</p>
+                  </div>
                 </div>
-                <div className='flex-1 h-full border border-green-500'></div>
+                <div className='flex-1 h-full p-5 pt-10 relative z-50'>
+                  <div className='flex items-center justify-between border border-amber-400'>
+                    <p className='text-white text-base leading-none'>Planto</p>
+                    <div className='flex items-center gap-2'>
+                      <p className='text-white text-[9px] leading-none'>.</p>
+                      <p className='text-white text-[9px] leading-none'>.</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className='w-full h-8 bg-red-400'>
+              <div className='w-full h-8 bg-neutral-900'>
 
               </div>
             </div>
