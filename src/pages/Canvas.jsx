261167870d4ede91ef87ff2858eee5d2e9bb4f6c
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-08-31 12:50:20 +0800

    progress for gardening UI

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { Tooltip } from '../components'
-import { TbSmartHome, TbReload, TbWifi, TbPlayerPlayFilled, TbBatteryVertical1Filled } from "react-icons/tb"
+import { TbSmartHome, TbMenu2, TbGridDots, TbSquareRounded, TbChevronLeft, TbBatteryVerticalFilled } from "react-icons/tb"
+import { PiWifiHighFill, PiCellSignalFullFill } from "react-icons/pi"
+import { BiSolidBattery } from "react-icons/bi"
 
 // Recording Device
 const Canvas = () => {
@@ -29,12 +31,12 @@ const Canvas = () => {
                 <img src='https://plus.unsplash.com/premium_photo-1728566648330-7ba0d675f3c6?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='sample' className='size-full object-cover absolute z-40'></img>
                 <div className='absolute -top-5.5 -translate-x-1/2 left-1/2  bg-neutral-950 h-17 w-[120%] z-40 blur-md' />
                 <div className='absolute -top-5.5 -translate-x-1/2 left-1/2  bg-neutral-950/90 h-30 w-[120%] z-40 blur-2xl' />
-                <div className='flex items-center justify-between absolute py-1.5 px-2.25 w-full bg-neutral-950 z-50 border border-amber-400'>
-                  <p className='text-white text-[9px] leading-none'>14:19</p>
-                  <div className='flex items-center gap-2'>
-                    <p className='text-white text-[9px] leading-none'>.</p>
-                    <p className='text-white text-[9px] leading-none'>.</p>
-                    <p className='text-white text-[9px] leading-none'>.</p>
+                <div className='flex items-center justify-between absolute py-1.5 px-2.25 w-full bg-neutral-950 z-50'>
+                  <p className='text-neutral-300 text-[9px] leading-none'>14:19</p>
+                  <div className='flex items-center gap-0.25'>
+                    <PiWifiHighFill className='size-2.5 text-neutral-200' />
+                    <PiCellSignalFullFill className='size-2.5 text-neutral-200 mt-[1.8px]' />
+                    <BiSolidBattery className='size-2.5 text-neutral-200 -rotate-90' />
                   </div>
                 </div>
                 <div className='flex flex-col justify-between h-full p-5 pt-10 relative z-50'>
@@ -66,7 +68,13 @@ const Canvas = () => {
                   </div>
                 </div>
               </div>
-              <div className='w-full h-8 bg-neutral-900'>
+              <div className='flex items-center justify-between w-full px-8 h-7.5 bg-neutral-900'>
+                <TbGridDots className='size-3.25 stroke-[2px] text-neutral-50' />
+                <div className='flex items-center gap-6'>
+                  <TbMenu2 className='size-3.25 stroke-[2px] text-neutral-50 rotate-90' />
+                  <TbSquareRounded className='size-3.25 stroke-[2px] text-neutral-50' />
+                  <TbChevronLeft className='size-3.25 stroke-[2px] text-neutral-50' />
+                </div>
               </div>
             </div>
           </div>
