c9955ac031fcddfbb7844337d7241806a788a53c
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-09-04 17:24:32 +0800

    Add UI007 component with MonteCarlo font and styles

---
+++ src/components/dumps/007/index.js
@@ -0,0 +1,13 @@
+import UI007 from './UI007.jsx'
+// import Skeleton007 from './Skeleton007.jsx'
+import UI007Code from './UI007.jsx?raw'
+// import Skeleton007Code from './Skeleton007.jsx?raw'
+import UI007CSS from './styles.css?raw'
+
+export {
+    UI007,
+    // Skeleton007,
+    UI007Code,
+    // Skeleton007Code,
+    UI007CSS
+}
