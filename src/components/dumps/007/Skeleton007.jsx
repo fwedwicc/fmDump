38f9cdd4cf6c3079899e257d7b926183138d04fe
fwedwicc <moreno.frederick.capiral@gmail.com>
2026-09-08 16:20:28 +0800

    Refactor Skeleton007 component styles for consistency and improved layout

--- src/components/dumps/007/Skeleton007.jsx
+++ src/components/dumps/007/Skeleton007.jsx
@@ -19,7 +19,7 @@ const Skeleton007 = () => {
                     <div className='flex flex-col size-full border rounded-[16px] overflow-hidden'>
                         <div className='relative w-full h-full flex-1 flex-col'>
                             <img src='https://placehold.co/30x30' alt='sample' className='size-full object-cover absolute z-40'></img>
-                            <div className='flex items-center justify-between absolute py-1.5 px-2.25 w-full border-b z-50'>
+                            <div className='flex items-center justify-between absolute py-1.5 px-2.25 w-full border-b bg-white z-50'>
                                 <p className='text-[9px] leading-none'>14:19</p>
                                 <div className='flex items-center gap-0.25'>
                                     <PiWifiHighFill className='size-2.5' />
@@ -33,78 +33,78 @@ const Skeleton007 = () => {
                                         <p className='text-[17px] leading-none tracking-tight'>pl<span className='montecarlo-font text-[28px] leading-none'>a</span><span className='italic leading-none'>n</span>to</p>
                                         <div className='flex items-center gap-2'>
                                             <TbBell className='size-4.5 stroke-[1.5px]' />
-                                            <TbSettings className='size-4.5 text-neutral-300 stroke-[1.5px]' />
+                                            <TbSettings className='size-4.5 stroke-[1.5px]' />
                                         </div>
                                     </div>
                                     <div className='flex items-center justify-between'>
                                         <div className='flex items-center gap-2'>
-                                            <div className='flex items-center rounded-full bg-black/40 divide-x divide-neutral-400/30'>
+                                            <div className='flex items-center rounded-full border divide-x'>
                                                 <div className='flex items-center justify-center pr-1.5 pl-2.5 py-1.25 overflow-hidden'>
-                                                    <p className='text-neutral-300 text-xs leading-none'>Fred</p>
+                                                    <p className='text-xs leading-none'>Fred</p>
                                                 </div>
                                                 <div className='flex items-center justify-center pr-2.5 pl-2 py-1.25 overflow-hidden'>
-                                                    <TbPencil className='size-4 text-neutral-300 stroke-[1.5px]' />
+                                                    <TbPencil className='size-4 stroke-[1.5px]' />
                                                 </div>
                                             </div>
-                                            <div className='flex items-center gap-1 py-1.75 px-2.75 pr-3 rounded-full bg-black/50'>
-                                                <TbHeartFilled className='size-3.25 text-[#DF732D]' />
-                                                <p className='text-neutral-300 text-xs leading-none'>82%</p>
+                                            <div className='flex items-center gap-1 py-1.75 px-2.75 pr-3 rounded-full border'>
+                                                <TbHeartFilled className='size-3.25' />
+                                                <p className='text-xs leading-none'>82%</p>
                                             </div>
                                         </div>
-                                        <div className='size-7 flex items-center justify-center rounded-full bg-neutral-700/40 -mr-[4px]'>
-                                            <TbSquareRoundedChevronsDown className='size-5 text-neutral-300 stroke-[1.5px]' />
+                                        <div className='size-7 flex items-center justify-center rounded-full border -mr-[4px]'>
+                                            <TbSquareRoundedChevronsDown className='size-5 stroke-[1.5px]' />
                                         </div>
                                     </div>
                                 </div>
                                 <div className='flex items-end justify-between'>
-                                    <div className='relative flex flex-col items-center gap-2.25 p-3 min-w-[7rem] bg-white rounded-xl'>
-                                        <TbChevronRight className='size-4 stroke-[2px] text-neutral-800 absolute right-1.5 top-2.25' />
+                                    <div className='relative flex flex-col items-center gap-2.25 p-3 min-w-[7rem] border rounded-xl'>
+                                        <TbChevronRight className='size-4 stroke-[2px] absolute right-1.5 top-2.25' />
                                         <div className='flex items-center justify-between w-full mb-1'>
-                                            <p className='leading-none font-bold text-8px] text-neutral-800'>Condition</p>
+                                            <p className='leading-none font-bold text-8px] mb-1'>Condition</p>
                                         </div>
                                         <div className='flex items-center w-full gap-2'>
-                                            <div className='grid place-content-center pl-[0.8px] size-[26.5px] bg-[#6F888B] text-white rounded-full'>
+                                            <div className='grid place-content-center pl-[0.8px] size-[26.5px] border rounded-full'>
                                                 <MdOutlineWaterDrop className='size-4.25' />
                                             </div>
                                             <div className='space-y-0.5'>
-                                                <p className='text-[7px] text-neutral-400 leading-none'>Water</p>
+                                                <p className='text-[7px] leading-none'>Water</p>
                                                 <p className='text-[11.5px] font-medium leading-none'>90%</p>
                                             </div>
                                         </div>
                                         <div className='flex items-center w-full gap-2'>
-                                            <div className='grid place-content-center pl-[0.8px] size-[26.5px] bg-[#EC9967] text-white rounded-full'>
+                                            <div className='grid place-content-center pl-[0.8px] size-[26.5px] border rounded-full'>
                                                 <TbSun className='size-4.25 stroke-[2px]' />
                                             </div>
                                             <div className='space-y-0.5'>
-                                                <p className='text-[7px] text-neutral-400 leading-none'>Light</p>
+                                                <p className='text-[7px] leading-none'>Light</p>
                                                 <p className='text-[11.5px] font-medium leading-none'>30%</p>
                                             </div>
                                         </div>
                                         <div className='flex items-center w-full gap-2'>
-                                            <div className='grid place-content-center pl-[0.8px] size-[26.5px] bg-[#9CA88D] text-white rounded-full'>
+                                            <div className='grid place-content-center pl-[0.8px] size-[26.5px] border rounded-full'>
                                                 <TbTemperature className='size-4.25 stroke-[2px]' />
                                             </div>
                                             <div className='space-y-0.5'>
-                                                <p className='text-[7px] text-neutral-400 leading-none'>Temperture</p>
+                                                <p className='text-[7px] leading-none'>Temperture</p>
                                                 <p className='text-[11.5px] font-medium leading-none'>24°</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='flex items-center gap-2.5 bg-neutral-900 px-1.25 py-[4.5px] pr-4.5 rounded-full'>
-                                        <div className='px-5 py-[7.5px] bg-neutral-100 rounded-full'>
-                                            <p className='text-neutral-900 font-bold text-[9px] leading-none'>Home</p>
+                                    <div className='flex items-center gap-2.5 border px-1.25 py-[4.5px] pr-4.5 rounded-full'>
+                                        <div className='px-5 py-[7.5px] border rounded-full'>
+                                            <p className='font-bold text-[9px] leading-none'>Home</p>
                                         </div>
-                                        <p className='text-white text-[9px] leading-none'>Community</p>
+                                        <p className='text-[9px] leading-none'>Community</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className='flex items-center justify-between w-full px-8 h-7.75 bg-neutral-900'>
-                            <TbGridDots className='size-3.5 stroke-[2px] text-neutral-50' />
+                        <div className='flex items-center justify-between w-full px-8 h-7.75 border-t bg-white'>
+                            <TbGridDots className='size-3.5 stroke-[2px]' />
                             <div className='flex items-center gap-6'>
-                                <TbMenu2 className='size-3.5 stroke-[2px] text-neutral-50 rotate-90' />
-                                <TbSquareRounded className='size-3.5 stroke-[2px] text-neutral-50' />
-                                <TbChevronLeft className='size-3.5 stroke-[2px] text-neutral-50' />
+                                <TbMenu2 className='size-3.5 stroke-[2px] rotate-90' />
+                                <TbSquareRounded className='size-3.5 stroke-[2px]' />
+                                <TbChevronLeft className='size-3.5 stroke-[2px]' />
                             </div>
                         </div>
                     </div>
